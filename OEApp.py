7466f24e8ff70fa5dbf8b7f6aec7f260724e035b
AFOEK <felix.montalfu@gmail.com>
2025-03-10 13:13:17 -0400

    Adding call 511 and fire department frames

--- OEApp.py
+++ OEApp.py
@@ -84,11 +84,39 @@ def set_911_frame(frame):
 
 def set_511_frame(frame):
     #Call 511 frames
-    pass
+    label_511 = tk.Label(frame, text="Call 511", font=("Arial 20"), cursor="hand2", fg="red")
+    def on_click_phone(e):
+        label_511.config(fg="darkblue")
+        try:
+            webbrowser.open("tel:511")
+        except Exception as ex:
+            frame.clipboard_clear()
+            frame.clipboard_append("511")
+            show_toast(frame.winfo_toplevel(), "Number copid to clipboard !")
+        label_511.after(200, lambda: label_511.config(fg="blue"))
+
+    label_511.bind("<Button-1>", on_click_phone)
+    label_511.bind("<Enter>", lambda e: label_511.config(fg="darkblue"))
+    label_511.bind("<Leave>", lambda e: label_511.config(fg="blue"))
+    label_511.pack(pady=20)
 
 def set_fd_frame(frame):
     #Call fire dept frames
-    pass
+    label_fd = tk.Label(frame, text="Call Fire department", font=("Arial 20"), cursor="hand2", fg="red")
+    def on_click_phone(e):
+        label_fd.config(fg="darkblue")
+        try:
+            webbrowser.open("tel:+19054533311")
+        except Exception as ex:
+            frame.clipboard_clear()
+            frame.clipboard_append("+19054533311")
+            show_toast(frame.winfo_toplevel(), "Number copid to clipboard !")
+        label_fd.after(200, lambda: label_fd.config(fg="blue"))
+
+    label_fd.bind("<Button-1>", on_click_phone)
+    label_fd.bind("<Enter>", lambda e: label_fd.config(fg="darkblue"))
+    label_fd.bind("<Leave>", lambda e: label_fd.config(fg="blue"))
+    label_fd.pack(pady=20)
 
 def set_weather_frame(frame):
     #Get weather update
